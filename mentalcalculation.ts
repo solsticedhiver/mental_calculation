6dcf0d5832229a3d137a4f3d3a76320ca69f9f9a
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2011-10-06 17:54:40 +0200

    Update Changelog
    * Update LISEZMOI
    * Include a default linguist ts file

---
+++ mentalcalculation.ts
@@ -0,0 +1,226 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="settings.py" line="21"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="34"/>
+        <source>Number of digits for each number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="35"/>
+        <source>Number of &amp;digits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="54"/>
+        <source>How many to add ?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="55"/>
+        <source>Number of &amp;rows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="73"/>
+        <source>Allow/disallow subtractions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="74"/>
+        <source>&amp;Subtractions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="80"/>
+        <source>Timeout in ms between number disappearing and next number appearing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="81"/>
+        <source>&amp;Timeout (ms)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="99"/>
+        <source>Time the number is displayed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="100"/>
+        <source>&amp;Flash (ms)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="130"/>
+        <source> &lt;p style=&apos;white-space:pre&apos;&gt;&lt;em&gt;&lt;strong&gt;espeak&lt;/strong&gt;&lt;/em&gt; is needed for speech synthesis&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="131"/>
+        <source>S&amp;peech Synthesis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="136"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; color:#ff0000;&quot;&gt;Error&lt;/span&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;: espeak&lt;/span&gt; executable has not been found!&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- this is because you need to &lt;span style=&quot; font-style:italic;&quot;&gt;install&lt;/span&gt; it&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- or because it&apos;s installed in a &lt;span style=&quot; font-style:italic;&quot;&gt;non-standard place&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Check the &lt;span style=&quot; font-weight:600;&quot;&gt;README&lt;/span&gt; to help you fix either of the problems.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="152"/>
+        <source>Pronounce each digit of a number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="153"/>
+        <source>One digit at a time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="158"/>
+        <source>In this mode, you don&apos;t have to enter an answer, it&apos;s &quot;hands free&quot;;
+the solution is displayed, and a new sequence begins,
+and so on ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="161"/>
+        <source>&amp;Continuous &quot;hands-free&quot; mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.py" line="165"/>
+        <source>Full screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="mentalcalculation.py" line="409"/>
+        <source>&amp;Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mentalcalculation.py" line="410"/>
+        <source>Stop the sequence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mentalcalculation.py" line="571"/>
+        <source>&amp;Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mentalcalculation.py" line="572"/>
+        <source>Start a sequence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mentalcalculation.py" line="532"/>
+        <source>The correct answer is %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mentalcalculation.py" line="499"/>
+        <source>Mental Calculation %1/%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main.py" line="21"/>
+        <source>Mental Calculation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="36"/>
+        <source>#</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="53"/>
+        <source>+99999</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="61"/>
+        <source>&amp;Your answer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="77"/>
+        <source>Type here your answer (the total)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="86"/>
+        <source>The total is </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="93"/>
+        <source>Start a sequence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="94"/>
+        <source>&amp;Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="99"/>
+        <source>Replay the last sequence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="100"/>
+        <source>Replay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="105"/>
+        <source>Check your answer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="106"/>
+        <source>&amp;Check</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="112"/>
+        <source>Go to Setttings dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="113"/>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="119"/>
+        <source>Exit the program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="120"/>
+        <source>&amp;Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
