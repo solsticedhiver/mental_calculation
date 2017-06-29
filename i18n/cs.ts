97028c437facdc03518d5859cdeb62cb34aac914
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2017-06-29 20:25:41 +0200

    Update cs.ts

--- i18n/cs.ts
+++ i18n/cs.ts
@@ -79,7 +79,7 @@ p, li { white-space: pre-wrap; }
         <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Sans Serif'; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; color:#ff0000;&quot;&gt;Error&lt;/span&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;: espeak&lt;/span&gt; spustitelný soubor nebyl nalezen!&lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- to proto, že jej musíte &lt;span style=&quot; font-style:italic;&quot;&gt;nainstalovat&lt;/span&gt; it&lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- nebo proto, že je nainstalován v &lt;span style=&quot; font-style:italic;&quot;&gt;nestandardním umístění&lt;/span&gt;&lt;/p&gt;
@@ -196,8 +196,8 @@ and so on ...</source>
     </message>
     <message>
         <location filename="main.py" line="95"/>
-        <source>&amp;Replay</source>
-        <translation>&amp;Přehrát znovu</translation>
+        <source>Replay</source>
+        <translation>Přehrát znovu</translation>
     </message>
     <message>
         <location filename="main.py" line="100"/>
@@ -216,7 +216,7 @@ and so on ...</source>
     </message>
     <message>
         <location filename="main.py" line="108"/>
-        <source>S&amp;ettings</source>
+        <source>&amp;Settings</source>
         <translation>&amp;Nastavení</translation>
     </message>
     <message>
